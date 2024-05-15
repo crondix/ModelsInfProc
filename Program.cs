ea8e83bd4a0f7af2abf51a49743870dd1bc486ed
Алексей Дударев <Dudarev.Aleksey@orenmin.onmicrosoft.com>
2024-05-15 11:49:41 +0500

    Выполняет симуляцию 1000 по 200

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ class Operator
 
     public int QueueLength => this._queueCalls.Count;
     public int nowTask => _nowTask;
-    public bool isQueueFull => this._queueCalls.Count < 2;
+    public bool isQueueFull => this._queueCalls.Count >= 2;
     public bool tryAddNewTask(int taskCompletionTime)
     {
         if (this._queueCalls.Count<2)
@@ -78,7 +78,13 @@ class Program
 {
     static void Main()
     {
-        Operator[] operators = new Operator[]
+        const int callsIteration = 201;
+        const int iteration = 1;
+
+        int tendenied = 0;
+        for (int g = 0; g < iteration; g++)
+        {
+            Operator[] operators = new Operator[]
         {
             new Operator (),
             new Operator (),
@@ -88,44 +94,51 @@ class Program
         };
           Random random = new Random();
        
-        int timeToNewRequest () { return random.Next(2, 4); };
-        int newTaskTime() { return random.Next(40, 50); };
+        int timeToNewRequest () { return random.Next(2, 9); };
+        int newTaskTime() { return random.Next(10, 51); };
         //operators[0].tryAddNewTask(newTaskTime());
-        int denied=0;
-        for (int i=1; i<50; i++)
-        {
-            int time = timeToNewRequest();
-            Console.WriteLine("Tick time:" + time);
-            for (int k = 0; k < operators.Length; k++)
+        
+        
+            int denied = 0;
+            for (int i = 1; i < callsIteration; i++)
             {
-                operators[k].Tick(time);
-                Console.WriteLine($"{k+1} operator task now:{operators[k].nowTask} queue langth:{operators[k].QueueLength}") ;
-            }
-            try
-            {
-                int minLoadId = getMinLoadId(operators);
-                int TaskTime = newTaskTime();
-                if (minLoadId >= 0 && operators[minLoadId].tryAddNewTask(TaskTime)) {
-                    
-                    Console.WriteLine($"{i} call for {minLoadId + 1} operator, task time:{TaskTime}, task at queue:{operators[minLoadId].QueueLength}");
-
-                }
-                else
+                int time = timeToNewRequest();
+                Console.WriteLine("Tick time:" + time);
+                for (int k = 0; k < operators.Length; k++)
                 {
-                    Console.WriteLine($"{i} itaration denied");
-                    denied++;
+                    operators[k].Tick(time);
+                    Console.WriteLine($"{k + 1} operator task now:{operators[k].nowTask} queue langth:{operators[k].QueueLength}");
                 }
-               
-               
-                Console.WriteLine($"----------------------------------------");
+                try
+                {
+                    int minLoadId = getMinLoadId(operators);
+                    int TaskTime = newTaskTime();
+                    if (minLoadId >= 0 && operators[minLoadId].tryAddNewTask(TaskTime))
+                    {
+
+                        Console.WriteLine($"{i}  CALL WAS RECEIVED AND DISTRIBUTED FOR {minLoadId + 1} OPERATOR, TASK TIME:{TaskTime}".ToUpper());
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i} itaration denied");
+                        denied++;
+                    }
+
+
+                    Console.WriteLine($"----------------------------------------");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }
-           
+            Console.WriteLine($"all denied calls:{denied}");
+            tendenied += denied;
         }
-        Console.WriteLine($"all denied calls:{denied}");
+        Console.WriteLine($"среднее за 1000:{tendenied/1000}");
+
 
 
         static int getMinLoadId(Operator[] operators)
@@ -145,12 +158,16 @@ class Program
 
             for (int i = 0; i < operators.Length; i++)
             {
-                int load = operators[i].QueueLength + (operators[i].isQueueFull ? 1 : 0);
-                if (load < minLoad)
+                if (!operators[i].isQueueFull)
                 {
-                    minLoad = load;
-                    minLoadId = i;
+                    int load = operators[i].QueueLength;
+                    if (load < minLoad)
+                    {
+                        minLoad = load;
+                        minLoadId = i;
+                    }
                 }
+               
             }
             return minLoadId;
         }
